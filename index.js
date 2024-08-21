3d2a270f871d734f8b9f1769eca511af6afe9570
Hassaan-Saleem <hassaansaleem486@gmail.com>
2024-08-21 19:53:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter First Number:",
+        type: "number",
+        name: "firstNumber"
+    },
+    {
+        message: "Enter Second Number:",
+        type: "number",
+        name: "secondNumber"
+    },
+    {
+        message: "Select a Operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log("Your value is " + Number(answer.firstNumber + answer.secondNumber));
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Your value is " + Number(answer.firstNumber - answer.secondNumber));
+}
+else if (answer.operator === "Multiplication") {
+    console.log("Your value is " + Number(answer.firstNumber * answer.secondNumber));
+}
+else if (answer.operator === "Division") {
+    console.log("Your value is " + Number(answer.firstNumber / answer.secondNumber));
+}
+else {
+    console.log("Please Select a valid operator");
+}
